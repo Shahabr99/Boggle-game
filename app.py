43686ed7b1d6a453fc06f5c222c8921e2083b88b
Shahabr99 <sean.rohani9@gmail.com>
2023-04-04 18:33:52 -0700

    added the board to the template

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, redirect, session
 from boggle import Boggle
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Whatever'
 boggle_game = Boggle()
 
 @app.route('/')
 def create_board():
     board = boggle_game.make_board()
-    raise
+    session['gameboard'] = board
+    
     return render_template('game.html', board = board)
